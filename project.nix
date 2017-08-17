6c16a583862fd24a74140da5584a0bd385ef315d
Alexander Svyazin <guybrush@live.ru>
2017-08-17 22:53:00 +0500

    extract project.nix from shell.nix

---
+++ project.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, attoparsec, base, blaze-builder
+      , bytestring, conduit, conduit-combinators, conduit-extra
+      , exceptions, http-client, http-conduit, http-types, lens, stdenv
+      , text, transformers
+      }:
+      mkDerivation {
+        pname = "hs-couchdb";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          aeson attoparsec base blaze-builder bytestring conduit
+          conduit-combinators conduit-extra exceptions http-client
+          http-conduit http-types lens text transformers
+        ];
+        testHaskellDepends = [ base ];
+        homepage = "https://github.com/asvyazin/hs-couchdb#readme";
+        description = "Minimal interface to CouchDB";
+        license = stdenv.lib.licenses.bsd3;
+        doHaddock = false;
+        doCheck = false;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+in
+  haskellPackages.callPackage f {}
